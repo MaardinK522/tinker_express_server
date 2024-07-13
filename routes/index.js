28b2f5cea58ee928eb4c0fb8eb61ba0346c9e294
chitra pandey <chitrapandey705@gmail.com>
2024-07-13 14:26:19 +0530

    chitra has successfully configured the backend with the frontend using github

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Index page of the Tinker Bolt.'});
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Index page of the Tinker Bolt." });
 });
 
-router.get('/home_page', function (req, res, next) {
-    res.render('home_page');
+router.get("/home_page", function (req, res, next) {
+  res.render("home_page");
 });
 
 module.exports = router;
